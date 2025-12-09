c5416126c0eb0a3914fb54b07d9d876093f3b1ea
labasse <labasse@gmail.com>
2025-12-09 10:40:00 +0100

    init regular list - green

--- app/src/main/java/labotdd/Laboratory.java
+++ app/src/main/java/labotdd/Laboratory.java
@@ -1,10 +1,25 @@
 package labotdd;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Laboratory {
     public Laboratory(String[] substances) {
-        throw new IllegalArgumentException("Substance list cannot be empty");
+        if(substances.length == 0) {
+            throw new IllegalArgumentException("Substance list cannot be empty");
+        }
+        for(var s : substances) {
+            if(substanceList.contains(s)) {
+                throw new IllegalArgumentException("Duplicate substance: " + s);
+            }
+            substanceList.add(s);
+        }
     }
     public double getQuantity(String substance) {
-        return -1.0;
+        if(!substanceList.contains(substance)) {
+            throw new IllegalArgumentException("Substance not found: " + substance);
+        }
+        return 0;
     }
+    private List<String> substanceList = new ArrayList<>();
 }

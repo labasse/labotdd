d3b33c0f8e7e2b00cd4ff95a435f58bcc259d00d
labasse <labasse@gmail.com>
2025-12-09 10:28:41 +0100

    init empty list - tdd green/refactor

--- app/src/main/java/labotdd/Laboratory.java
+++ app/src/main/java/labotdd/Laboratory.java
@@ -2,6 +2,6 @@ package labotdd;
 
 public class Laboratory {
     public Laboratory(String[] substances) {
-        
+        throw new IllegalArgumentException("Substance list cannot be empty");
     }
 }
